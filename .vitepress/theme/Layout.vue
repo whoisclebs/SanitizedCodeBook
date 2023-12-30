7c654f37636242ffdb8097be0b912c28446e575a
whoisclebs <clebson.augusto@dcx.ufpb.br>
2023-12-30 13:24:33 -0300

    chore: Add initial project base 30-12-2023

---
+++ .vitepress/theme/Layout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useData } from 'vitepress'
+
+// https://vitepress.dev/reference/runtime-api#usedata
+const { site, frontmatter } = useData()
+</script>
+
+<template>
+    <Content />
+</template>
